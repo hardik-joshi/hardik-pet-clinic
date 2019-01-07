7cc4cd3080869c0ffb5d3a9b0d55688142414077
Hardik Joshi <joshi.hardik654@gmail.com>
2019-01-08 00:26:33 +0530

    Initial Project Add

---
+++ src/main/java/com/springframwork/hardikpetclinic/HardikPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.springframwork.hardikpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardikPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardikPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/springframwork/hardikpetclinic/HardikPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springframwork.hardikpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HardikPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
